c4060f3e7d0e0a7e5f2a5bb700450ea04eb5b0f1
TheRealSmaft <mrsmaft@gmail.com>
2018-01-04 08:51:21 -0600

    Improved Styles / Layout

--- app/components/elements/bio/index.js
+++ app/components/elements/bio/index.js
@@ -3,22 +3,59 @@ import React from 'react';
 import styles from '../../../styles/bio.css';
 
 class Bio extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentAboutParagraph: 0
+		}
+
+		this.aboutTextArray = [
+			"My name is Matthew Brubaker Smith and I am a Graphic and Web Designer. Though to be honest, my skill set is more broad and varied than that title conveys.",
+			"Fuck you"
+		];
+
+		this.buttonTextArray = [
+			"Read More",
+			"Check My Skillz"
+		];
+	}
+
+	getNextAboutParagraph() {
+		let nextP;
+
+		if(this.state.currentAboutParagraph + 1 >= this.aboutTextArray.length) 
+		{
+			nextP = 0;
+		}
+		else
+		{
+			nextP = this.state.currentAboutParagraph + 1;
+		}
+
+		this.setState({
+			...this.state,
+			currentAboutParagraph: nextP
+		});
+	}
+
 	render() {
 		return(
 			<div
 				className = {styles.bio}
 			>
 				<h1>
-					Matthew Brubaker Smith
+					Matthew Brubaker Smith {this.props.farts}
 				</h1>
 				<h3>
 					Web&nbsp;Developer &bull; Graphic&nbsp;Designer
 				</h3>
 				<p>
-					My name is Matthew Brubaker Smith and I am a Graphic and Web Designer. Though to be honest, my skill set is more broad and varied than that title conveys.
+					{this.aboutTextArray[this.state.currentAboutParagraph]}
 				</p>
-				<button>
-					BUT WAIT THEREZ MORE
+				<button
+					onClick = {() => this.getNextAboutParagraph()}
+				>
+					{this.buttonTextArray[this.state.currentAboutParagraph]}
 				</button>
 			</div>
 		)
